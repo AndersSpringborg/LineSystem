39a3e8e821a3dbe9ff8695044a4c5ae9a6d0db70
Anders Aaen Springborg <asprin18@student.aau.dk>
2019-04-03 17:23:13 +0200

    First Properties

---
+++ LineSystem/Core/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                User kurt = new User(1, "Anders", "Springborg", Console.ReadLine());
+                Console.WriteLine(kurt);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("En værdi må ikke være ingenting");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LineSystem/Core/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    class User
+    {
+        class User
+        {
+            // Backing Fields
+            private string _firstName;
+            private string _lastName;
+            private string _userName;
+            
+            // Properties
+            public uint ID { get; set; }
+            public string FirstName { get => _firstName; set { CheckSetter(value, v => v.All(Char.IsLetter), ref _firstName); } }
+            public string LastName { get => _lastName; set { CheckSetter(value, v => v.All(Char.IsLetter), ref _lastName); } }
+            public string UserName
+            {
+                get
+                {
+                    return _userName;
+                }
+                set
+                {
+                    CheckSetter(value, v => v.All(Char.IsLetterOrDigit), ref _userName);
+
+                    //if (value.All(Char.IsLetterOrDigit))
+                    //    _userName = value;
+                    //else
+                    //{
+                    //    _userName = "Forkert navn";
+                    //    throw new Exception();
+                    //}
+                }
+            }
+
+            // Methods
+            public User(uint iD, string firstName, string lastName, string userName)
+            {
+                ID = iD;
+                FirstName = firstName;
+                LastName = lastName;
+                UserName = userName;
+            }
+
+            public override string ToString() => $"{FirstName} {LastName} \n\tID: {ID.ToString().PadLeft(10)} Username: {UserName}";
+
+            // Genereic function, that valides an input. Validates in line with the methods parameter
+            private void CheckSetter<T>(T value, Predicate<T> checkFunction, ref T field)
+            {
+                field = checkFunction(value) ? value : throw new ArgumentException("value stemmer ikke overens med krav (checkfunction)");
+                //if (checkFunction(value))
+                //{
+                //    field = value;
+                //}
+                //else
+                //{
+                //    throw new ArgumentException("value stemmer ikke overens med krav (checkfunction)");
+                //}
+            }
+        }
