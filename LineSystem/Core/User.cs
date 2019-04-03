266b936912f6dc83c3feb13f349809da9610e9b9
Anders Aaen Springborg <asprin18@student.aau.dk>
2019-04-03 17:27:22 +0200

    Fixed a typo with curly brackets

--- LineSystem/Core/User.cs
+++ LineSystem/Core/User.cs
@@ -8,59 +8,58 @@ namespace Core
 {
     class User
     {
-        class User
+        // Backing Fields
+        private string _firstName;
+        private string _lastName;
+        private string _userName;
+
+        // Properties
+        public uint ID { get; set; }
+        public string FirstName { get => _firstName; set { CheckSetter(value, v => v.All(Char.IsLetter), ref _firstName); } }
+        public string LastName { get => _lastName; set { CheckSetter(value, v => v.All(Char.IsLetter), ref _lastName); } }
+        public string UserName
         {
-            // Backing Fields
-            private string _firstName;
-            private string _lastName;
-            private string _userName;
-            
-            // Properties
-            public uint ID { get; set; }
-            public string FirstName { get => _firstName; set { CheckSetter(value, v => v.All(Char.IsLetter), ref _firstName); } }
-            public string LastName { get => _lastName; set { CheckSetter(value, v => v.All(Char.IsLetter), ref _lastName); } }
-            public string UserName
+            get
             {
-                get
-                {
-                    return _userName;
-                }
-                set
-                {
-                    CheckSetter(value, v => v.All(Char.IsLetterOrDigit), ref _userName);
-
-                    //if (value.All(Char.IsLetterOrDigit))
-                    //    _userName = value;
-                    //else
-                    //{
-                    //    _userName = "Forkert navn";
-                    //    throw new Exception();
-                    //}
-                }
+                return _userName;
             }
-
-            // Methods
-            public User(uint iD, string firstName, string lastName, string userName)
+            set
             {
-                ID = iD;
-                FirstName = firstName;
-                LastName = lastName;
-                UserName = userName;
-            }
+                CheckSetter(value, v => v.All(Char.IsLetterOrDigit), ref _userName);
 
-            public override string ToString() => $"{FirstName} {LastName} \n\tID: {ID.ToString().PadLeft(10)} Username: {UserName}";
-
-            // Genereic function, that valides an input. Validates in line with the methods parameter
-            private void CheckSetter<T>(T value, Predicate<T> checkFunction, ref T field)
-            {
-                field = checkFunction(value) ? value : throw new ArgumentException("value stemmer ikke overens med krav (checkfunction)");
-                //if (checkFunction(value))
-                //{
-                //    field = value;
-                //}
+                //if (value.All(Char.IsLetterOrDigit))
+                //    _userName = value;
                 //else
                 //{
-                //    throw new ArgumentException("value stemmer ikke overens med krav (checkfunction)");
+                //    _userName = "Forkert navn";
+                //    throw new Exception();
                 //}
             }
         }
+
+        // Methods
+        public User(uint iD, string firstName, string lastName, string userName)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            UserName = userName;
+        }
+
+        public override string ToString() => $"{FirstName} {LastName} \n\tID: {ID.ToString().PadLeft(10)} Username: {UserName}";
+
+        // Genereic function, that valides an input. Validates in line with the methods parameter
+        private void CheckSetter<T>(T value, Predicate<T> checkFunction, ref T field)
+        {
+            field = checkFunction(value) ? value : throw new ArgumentException("value stemmer ikke overens med krav (checkfunction)");
+            //if (checkFunction(value))
+            //{
+            //    field = value;
+            //}
+            //else
+            //{
+            //    throw new ArgumentException("value stemmer ikke overens med krav (checkfunction)");
+            //}
+        }
+    }
+}
