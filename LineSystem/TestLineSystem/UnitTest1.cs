b96ee1b87e1e4eeaf550a4073af856f82cda0b96
Anders Aaen Springborg <asprin18@student.aau.dk>
2019-04-13 15:40:59 +0200

    delegates implemented I unitTest begun

---
+++ LineSystem/TestLineSystem/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+
+namespace TestLineSystem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestUserClassConstructor()
+        {
+            User user1 = new User("Anders", "Springborg", "888_aaen", "aaspringborg@gmail.com");
+
+
+            Assert.AreEqual("Anders", user1.FirstName);
+            Assert.AreEqual("Springborg", user1.LastName);
+            Assert.AreEqual((UInt32)101, user1.MyId);
+            Assert.AreEqual("888_aaen", user1.UserName);
+        }
+    }
+}
